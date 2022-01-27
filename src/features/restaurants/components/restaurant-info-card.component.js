6c7f210b729d21c4dbcc820683a9d27178ca3dff
RamprasathRavichandran <47816256+RamprasathRavichandran@users.noreply.github.com>
2022-01-27 20:01:05 +0100

    just a commit

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import styled from "styled-components";
+
+import { StyleSheet } from "react-native";
+
+import { Card } from "react-native-paper";
+
+const Title = styled.Text`
+  padding: 16px;
+  color: red;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "SomeRestaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "250 some street",
+    isOpenNow = true,
+    rating = 3,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: { backgroundColor: "white" },
+  cover: { padding: 20, backgroundColor: "white" },
+  title: { padding: 25 },
+});
